0145b12a0b73bef08012c21d009e9af429ac6df4
DevanshParashar165 <parashardevansh165@gmail.com>
2024-12-17 13:09:29 +0530

    done if else, loop , function

--- script.js
+++ script.js
@@ -148,7 +148,77 @@
 // }
 // first();
 
-var age = 20;
-if(age>18){
-    document.write("<b>You are eligible to vote</b>");
+// var age = 20;
+// if(age>18){
+//     document.write("<b>You are eligible to vote</b>");
+// }
+
+// var grade = 'A';
+// document.write("Entering switch block<br>");
+// switch (grade) {
+//     case 'A':
+//         document.write("Good job<br>");
+//         break;
+
+//     case 'B':
+//         document.write("Pretty Good<br>");
+//         break;
+
+//     case 'C' :
+//         document.write("Passed<br>");
+//         break;   
+//     case 'D':
+//         document.write("Not so good<br>");
+//         break;
+
+//     case 'F':
+//         document.write("Failed<br>");
+//         break;   
+//     default:
+//         document.write("Unknown grade<br>");
+//         break; 
+// }
+// document.write("Exiting switch block");
+
+// var i;
+// for(i=0;i<3;i=i+1){
+//     console.log(i)
+// }
+
+// var count;
+// document.write("Starting Loop" + "<br>");
+// for(count=0;count<10;count++){
+//     document.write("Current Count : "+ count);
+//     document.write("<br>")
+// }
+// document.write("Loop Stopped!");
+
+// for(var i = 0;i<100;i++){
+//     if(i%2==0){
+//         console.log(i + " is an even number");
+//     }
+//     else{
+//          console.log(i + " is an odd number");
+//     }
+// }
+
+// var myArray = ["A","B","C"]
+// for(var i = 0;i<myArray.length;i++){
+//     console.log("The member of myArray in index "+i+" is "+myArray[i]);
+// }
+// var count = 0;
+// document.write("Starting Loop<br>");
+// while(count<10){
+//     document.write("Current Count : " + count + "<br>")
+//     count++;
+// }
+
+// document.write("Loop Stopped!");
+
+// function myFunction(){
+//     alert("Hello world");
+// }
+
+function sayHello(name,age){
+    document.write(name + " is "+ age +" year old");
 }
