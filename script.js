6c1ac6e428767296d420a7740a56212039ccd8da
DevanshParashar165 <parashardevansh165@gmail.com>
2024-12-19 16:27:26 +0530

    functions arrow_functions operators array

--- script.js
+++ script.js
@@ -331,5 +331,5 @@
 // var votable = (age<18)?"Too young":"Old enough";
 // console.log(votable)
 
-var myCollection = [1,"meera maam",true,{name:"Sanjeev",age:30},function(name){console.log(name)},"abhishek","bacchan","amitabh","bacchan"]
+var myCollection = [1,"meera maam",true,{name:"Sanjeev",age:30},function(name){console.log(name)},["abhishek","bacchan","amitabh","bacchan"]]
 console.log(myCollection)
